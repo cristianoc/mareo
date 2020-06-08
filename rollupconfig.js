e910463188d4ada15dc69e2b58e186fb4be110a5
Cristiano Calcagno <ccrisccris@gmail.com>
2020-06-08 09:10:53 +0200

    simplify rollup config -- helps with debugging

--- rollupconfig.js
+++ rollupconfig.js
@@ -1,8 +1,4 @@
 import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import replace from "rollup-plugin-replace";
-import { uglify } from "rollup-plugin-uglify";
-
 export default {
   input: "src/Mareo.js",
   output: {
@@ -11,20 +7,6 @@ export default {
     name: "main"
   },
   plugins: [
-    resolve(),
-    commonjs({
-      include: "node_modules/**",
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        "react-dom": ["render", "hydrate"],
-        react: ["createElement", "useReducer"]
-      }
-    }),
-    replace({
-      "process.env.NODE_ENV": "'production'"
-    }),
-    uglify()
+    resolve()
   ]
 };
